bf610c8cab3cfa00e3a31c196ce1060237edc596
Dhwty <clemwek@gmail.com>
2018-05-31 15:18:39 +0300

    Convert to mvc

---
+++ todoey/Data Models/items.swift
@@ -0,0 +1,14 @@
+//
+//  items.swift
+//  todoey
+//
+//  Created by Clement Wekesa on 31/05/2018.
+//  Copyright © 2018 Clement Wekesa. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
